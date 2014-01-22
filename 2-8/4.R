ec369b2590dc8f340f551240f8eef5ea9f8c82bc
nakulj <nakuljos@usc.edu>
2014-01-22 00:08:14 -0800

    Reorganised, finished a)

---
+++ 2-8/4.R
@@ -0,0 +1,10 @@
+
+#a
+flights<-read.csv("../Data/FlightDelays.csv")
+times<-flights$DepartTime
+times<-factor(times, levels= c("4-8am","8-Noon","Noon-4pm","4-8pm","8-Mid"),ordered= TRUE)
+table<-table(times)
+print(table)
+barplot(table)
+
+#b
