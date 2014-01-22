e0d5e106de6a0b72b8d50757abf180132f7078b9
nakulj <nakuljos@usc.edu>
2014-01-22 12:09:49 -0800

    More

--- 2-8/6.R
+++ 2-8/6.R
@@ -1,1 +1,10 @@
+spruces<-read.csv("../Data/Spruce.csv")
+
 #a
+changes<-spruces$Ht.Change
+summary(changes)
+hist(changes)
+avg<-mean(changes)
+sd<-sd(changes)
+hist(changes)
+plot(density(changes))
