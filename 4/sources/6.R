f649bd9707932cf9c6aba9437dc4dc7825bea52a
nakulj <nakuljos@usc.edu>
2014-01-25 11:54:33 -0800

    Done with 6

--- 4/sources/6.R
+++ 4/sources/6.R
@@ -10,5 +10,13 @@ qqnorm(Ht.change)
 qqline(Ht.change)
 
 #c
+yes<-subset(spruces, Fertilizer=="F")$Di.change
+no<-subset(spruces, Fertilizer=="NF")$Di.change
+boxplot(yes,no,names=c("In fertilized plots","Not in fertilized plots"))
 
+#d
+tapply(spruces$Di.change,spruces$Fertilizer,summary)
 
+#e
+Di.change=spruces$Di.change
+plot(Ht.change,Di.change)
