add04a2261e5d9bea816d17a2307a3fc42a4ce14
nakulj <nakuljos@usc.edu>
2014-02-09 15:52:35 -0800

    Skeleton

--- 7/sources/8.R
+++ 7/sources/8.R
@@ -1,1 +1,20 @@
+
+#Organise the data
 spruces<-read.csv("Spruce.csv")
+
+#Get observed test statistic
+m= 36
+n= 36
+means<- tapply(spruces$Ht.change, spruces$Competition, mean)
+m.avg= means[["C"]]
+n.avg= means[["NC"]]
+diff= m.avg-n.avg
+
+#Get resample test statistics
+tests= 10000
+passed=0
+for(i in 1:tests) {
+	if(testResample())
+		passed=passed+1
+}
+print(passed/tests)
