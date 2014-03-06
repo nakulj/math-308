9accc4a517e87baec14178dace621e4de22007d2
nakulj <nakuljos@usc.edu>
2014-03-05 19:57:13 -0800

    Trying to make pdfs

--- submission.sh
+++ submission.sh
@@ -3,12 +3,17 @@
 
 function zipdir () {
 	zip -r Submission$1.zip $2 -x \*.tex $2/img\*
-	#"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o temp.pdf $2/assignment$2.
+}
+
+function pdf () {
+	"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o temp.pdf --append $1/assignment$1.pdf
 }
 
 for i in ${@:2}
 do
 	zipdir $1 $i
+	pdf $i
 done
 echo "to Submission$1.zip"
+
 
