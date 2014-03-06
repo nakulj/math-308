7d0a940899336e359dddc0c025672745ba450cf8
nakulj <nakuljos@usc.edu>
2014-03-05 20:45:32 -0800

    git clear

--- submission.sh
+++ submission.sh
@@ -1,6 +1,3 @@
-#git clean -f -X
-
-
 function zipdir () {
 	zip -r Submission$1.zip $2 -x \*.tex $2/img\*
 }
@@ -9,6 +6,7 @@ function pdf () {
 	"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o print$1.pdf ${@:2}
 }
 
+git clean -f -X
 pdflist=""
 for i in ${@:2}
 do
