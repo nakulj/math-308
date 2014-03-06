77e848f6b33501a2e7a28f8b45ca311f70b3588c
nakulj <nakuljos@usc.edu>
2014-03-05 20:38:10 -0800

    I think this works

--- submission.sh
+++ submission.sh
@@ -6,11 +6,13 @@ function zipdir () {
 }
 
 function pdf () {
-	python "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o print$1.pdf $2/assignment$2.pdf
+	"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py -o print$1.pdf ${@:2}"
 }
 
+pdflist=""
 for i in ${@:2}
 do
 	zipdir $1 $i
-	pdf $1 $i
+	pdflist="$pdflist $i/assignment$i.pdf"
 done
+pdf $1 $pdflist
