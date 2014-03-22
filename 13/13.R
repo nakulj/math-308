610f648be33e9b213480f2cec0357f0dbbbe7271
nakulj <nakuljos@usc.edu>
2014-03-21 18:17:43 -0700

    13

--- 13/13.R
+++ 13/13.R
@@ -1,22 +1,33 @@
+# 'n' is the size of the sample to generate
+# 'tests' is the number of bootstrap resamples
 
-test<-function(n=100, tests= 10000) {
+rescale<-function(est,n) {
+	return(
+		(n-1)*est/n
+	)
+}
+
+test<-function(n=10, tests= 20000) {
 
 	sample<-rnorm(n)
 
-	unbiasedVar= var(sample)
-	biasedVar= (n-1)*unbiasedVar/n
+	est1= var(sample)
+	est2= rescale(est1,n)
 
-	tests= 10000
 	resamples<-matrix(
-		sample(sample, tests*length(sample), replace= TRUE),
+		sample(sample, tests*n, replace= TRUE),
 		nrow= tests
 	)
 
-	resamples.unbiasedVars<-apply(resamples,1,var)
-	resamples.biasedVars<-(n-1)*resamples.unbiasedVars/n
-	hist(resamples.unbiasedVars)
-	dev.new()
-	hist(resamples.biasedVars)
+	bsEst1= mean(apply(resamples,1,var))
+	bsEst2= rescale(bsEst1,n)
+	
+	bias1= bsEst1-est2
+	bias2= bsEst2-est1
+
+	print(bias1)
+	print(bias2)
+
 }
 
 test()
