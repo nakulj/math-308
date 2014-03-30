8085f16abc43f98b485ee51e3735cf788e474df7
nakulj <nakuljos@usc.edu>
2014-03-30 12:59:29 -0700

    Fixed submission script

--- submit.sh
+++ submit.sh
@@ -1,6 +1,7 @@
 git clean -f -X
-rm *.zip
-./submission.sh 1 1 3
-./submission.sh 2 2 4 5 6 7
-./submission.sh 3 8 9 10 11
-./submission.sh 4 12 13 14 15
+./compile.sh 1 1 3
+./compile.sh 2 2 4 5 6 7
+./compile.sh 3 8 9 10 11
+./compile.sh 4 12 13 14 15
+./compile.sh 5 16 17
+./compile.sh 6 18 19
