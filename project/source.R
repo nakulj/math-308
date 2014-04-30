c26e1b06e84cb0a85a354e7e2cadb41ca81e51fa
nakulj <nakuljos@usc.edu>
2014-04-30 10:52:58 -0700

    Fin

--- project/source.R
+++ project/source.R
@@ -1,4 +1,4 @@
-
+#Analysis
 
 datasets=c("data/Fall2013.csv","data/Spring2013.csv")
 usage=0
@@ -17,3 +17,11 @@ print(
 )
 
 #Cross validation
+fa12= read.csv("data/Fall2012.csv")
+fa12$USED=fa12$CONDUCTED + fa12$NOSHOW
+fa12$= (fa12$DOW==3 | fa12$DOW==4)
+usage<-tapply(fa12$USED,fa12$MT,sum)
+avail<-tapply(fa12$USED+fa12$EMPTY,fa12$MT,sum)
+print(
+	prop.test(usage,avail,alternative="less")
+)
